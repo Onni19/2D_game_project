bc160e10102764e15c31febb0c54e88570f3dade
DeXter <65320503+De-X-ter@users.noreply.github.com>
2022-12-26 11:06:07 +0600

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -4,21 +4,19 @@ import math
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, angle):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('images/bullet/Cannonball.png').convert_alpha()
-        w = self.image.get_width()
-        h = self.image.get_height()
-        self.image = pygame.transform.scale(self.image, (int(w * 0.04), int(h * 0.04)))
+        pygame.sprite.Sprite.__init__(self)  # calling super class
+        bullet_img = images_path + 'bullet-1.png'
+        img = Images()
+        self.image = img.image_process(bullet_img, BULLET_SIZE)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.angle = angle
-        self.speed = 13
-        # calculate speed
-        self.dx = math.cos(self.angle) * self.speed
-        self.dy = math.sin(self.angle) * self.speed
-        self.x = x
-        self.y = y
+        self.angle = math.radians(angle)  # convert input angle into radius
+        # self.angle = angle # convert input angle into radius
+        self.speed = BULLET_SPEED_INITIAL
+        # calculate the horizontal and vertical speed based on the angle
+        self.change_x_pos_dx = math.cos(self.angle) * self.speed
+        self.change_y_pos_dy = math.sin(self.angle) * self.speed
 
     def update(self, surface, w, h):
         # if check if bullet has gone off the screeen
