df88ffd08293da2c83252b35c3fd84e339f81785
DeXter <65320503+De-X-ter@users.noreply.github.com>
2023-01-12 16:49:53 +0600

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,25 +1,34 @@
+import pygame
 import math
-from images import *
 
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, angle):
-        pygame.sprite.Sprite.__init__(self)  # calling super class
-        bullet_img = images_path + 'bullet-1.png'
-        img = Images()
-        self.image = img.image_process(bullet_img, BULLET_SIZE)
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/bullet/Cannonball.png').convert_alpha()
+        w = self.image.get_width()
+        h = self.image.get_height()
+        self.image = pygame.transform.scale(self.image, (int(w * 0.04), int(h * 0.04)))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.angle = math.radians(angle)  # convert input angle into radius
-        # self.angle = angle # convert input angle into radius
-        self.speed = BULLET_SPEED_INITIAL
-        # calculate the horizontal and vertical speed based on the angle
-        self.change_x_pos_dx = math.cos(self.angle) * self.speed
-        self.change_y_pos_dy = math.sin(self.angle) * self.speed
+        #print(y)
+        self.angle = angle
+        self.speed = 13
+        # calculate speed
+        self.dx = math.cos(self.angle) * self.speed
+        self.dy = math.sin(self.angle) * self.speed
+        self.x = x
+        self.y = y
 
-    def update(self) -> None:
-        # move bullet
-        self.rect.x += self.change_x_pos_dx
-        self.rect.y += self.change_y_pos_dy
+    def update(self, surface, w, h):
+        # if check if bullet has gone off the screeen
+        if self.rect.right < 0 or self.rect.left > w or self.rect.bottom < 0 or self.rect.top > h:
+            self.kill()
+        self.x += self.dx
+        self.y += self.dy
+        self.rect.x = self.x
+        self.rect.y = self.y
+        #pygame.draw.rect(surface, (255, 255, 255), self.rect, 1)
+
 
